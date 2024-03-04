173afcdbe95eb6c190afa0f126cf6911f50e65fd
richardgomeza <2324256@gmail.com>
2024-03-04 00:03:17 -0300

    Crud Countries + database

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        //Data Annotations
+        [Display(Name = "País")] 
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+
+    }
+}
